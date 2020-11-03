be2ca65e717cdf8ad058c0b84e93655d54ced8c6
Tooz Wu <opeakwt@gmail.com>
2020-11-03 23:49:32 +0800

    Update $TON in Telegram Messenger

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -45,6 +45,10 @@ const HomeComponent = ({ className }) => {
           <Row className="content" id="news">
             <Title>News</Title>
             <Typography>
+              <Text>03.11.2020</Text> <Text strong> Community</Text>
+              <Paragraph>
+                Public test: rain / tip / giveaway $TON in Telegram Messenger; Time: 04.11.2020~06.11.2020 UTC+8 22:00; Place: telegram group < a href="https://t.me/ton_en"> @ton_en</ a>, < a href="https://t.me/ton_en"> @ton_cn</ a>.
+              </Paragraph>    
               <Text>14.10.2020</Text> <Text strong> Community</Text>
               <Paragraph>
                 1,Testnet4 Launch;2,New proposal: Modify Main network Supply,< a href="https://snapshot.page/#/ton/proposal/QmXhuKWbkfVyu8PazQz7pA349KFTJUanP7rntT7VoJVrDT"> Here</ a>
